30625fc274dfed82ceb73b08872d49f501074078
razumolla <rajuahmed9344@gmail.com>
2022-04-18 19:28:52 +0600

    About me Updated

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>My name is Razu</h2>
+        <div className='container mt-5 pt-2' style={{height: '65vh'}} >
+            <p>
+                Hi! I am Razu, a web developer focused on crafting great web experiences. Problem solving and Coding have been my passion since the days I started working with computers but I found myself in web development in 2022. I enjoy creating beautifully designed, intuitive and functional websites.
+            </p>
+            <p>
+                My future plan is to be a MERN stack web developer and to be good at DSA and Problem-solving.
+            </p>
         </div>
     );
 };
